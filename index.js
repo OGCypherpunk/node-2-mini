aac08280ec18a656b4a709fa2ec86c84f570eb7d
OGCypherpunk <chuck.devmtn@gmail.com>
2018-03-29 13:48:25 -0500

    updated airplan es.sql to include drop as well as other small changes in index.js

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
-require('dotenv').config();
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const massive = require("massive");
 
 const app = express();
-app.use( bodyParser.json() );
-app.use( cors() );
 
-const port = process.env.PORT || 3000
+massive(process.env.CONNECTION_STRING)
+  .then(dbInstance => {
+    app.set("db", dbInstance);
+    dbInstance.airplanes();
+    dbInstance.new_planes();
+  })
+  .catch(console.log);
 
+app.use(bodyParser.json());
+app.use(cors());
 
-massive(process.env.CONNECTION_STRING).then(dbInstance => {
-    console.log("dbInstance: " , dbInstance);
-})
+app.get("/api/planes", (req, res) => {
+  req.app
+    .get("db")
+    .get_planes()
+    .then(planes => res.status(200).json(planes));
+});
 
+app.get("/api/plane/:id", (req, res) => {
+  req.app
+    .get("db")
+    .run("select * from airplanes where plane_id = $1", [req.params.id])
+    .then(planes => res.status(200).json(planes));
+});
 
-
-
-
-
-app.listen( port , () => { console.log(`Server listening on port ${port}`); } );
-
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
